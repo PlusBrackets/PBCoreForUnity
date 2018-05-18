36c86338d584c4c85ca9ec4af363587837e55030
plusBrackets <liu997139467@gmail.com>
2018-05-18 20:29:05 +0800

    修改截图矩阵的取值

--- Assets/PBCore/Script/Utils/Screenshot.cs
+++ Assets/PBCore/Script/Utils/Screenshot.cs
@@ -17,7 +17,7 @@ namespace PBCore
         /// <param name="clarity">清晰度[0.01,1]</param>
         /// <param name="depth"></param>
         /// <returns></returns>
-        public static Texture2D FromCamera(Camera camera, Rect rect, float clarity = 1, TextureFormat textureFormat = TextureFormat.RGB24, bool mipmap = false, bool linear = false, int depth = 16)
+        public static Texture2D FromCamera(Camera camera, Rect rect, float clarity = 1, TextureFormat textureFormat = TextureFormat.RGB24, bool mipmap = true, bool linear = true, int depth = 16)
         {
             return FromCamera(new Camera[] { camera }, rect, clarity, textureFormat, mipmap, linear, depth);
         }
@@ -33,14 +33,14 @@ namespace PBCore
         /// <param name="linear"></param>
         /// <param name="depth"></param>
         /// <returns></returns>
-        public static Texture2D FromCamera(Camera[] cameras, Rect rect, float clarity = 1, TextureFormat textureFormat = TextureFormat.RGB24, bool mipmap = false, bool linear = false, int depth = 16)
+        public static Texture2D FromCamera(Camera[] cameras, Rect rect, float clarity = 1, TextureFormat textureFormat = TextureFormat.RGB24, bool mipmap = true, bool linear = true, int depth = 16)
         {
             clarity = Mathf.Clamp(clarity, 0.01f, 1f);
             rect.x *= clarity;
             rect.y *= clarity;
             rect.width *= clarity;
             rect.height *= clarity;
-            RenderTexture rt = new RenderTexture((int)(Screen.width * clarity), (int)(Screen.height * clarity), depth);
+            RenderTexture rt = new RenderTexture(Mathf.RoundToInt(Screen.width * clarity), Mathf.RoundToInt(Screen.height * clarity), depth);
             for (int i = 0; i < cameras.Length; i++)
             {
                 cameras[i].targetTexture = rt;
@@ -49,7 +49,7 @@ namespace PBCore
             }
             RenderTexture temp = RenderTexture.active;
             RenderTexture.active = rt;
-            Texture2D screenshot = new Texture2D((int)rect.width, (int)rect.height, textureFormat, mipmap, linear);
+            Texture2D screenshot = new Texture2D(Mathf.RoundToInt(rect.width), Mathf.RoundToInt(rect.height), textureFormat, mipmap, linear);
             screenshot.ReadPixels(rect, 0, 0);
             screenshot.Apply();
             RenderTexture.active = temp;
